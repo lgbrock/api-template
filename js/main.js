5b2726a1fa1212a97fb00b46ecdce424d8ef1b29
Logan Brock <logan.g.brock@gmail.com>
2021-01-30 13:57:20 -0600

    API practice qustions finished

--- js/main.js
+++ js/main.js
@@ -1,41 +1,26 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('#q').addEventListener('click', getFetch)
+document.querySelector('#a').addEventListener('click', showAnswer)
+
+let answer = ''
 
 function getFetch() {
-  const poke1 = document.querySelector('#poke1').value
-  const poke2 = document.querySelector('#poke2').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/' + poke1
-  const url2 = 'https://pokeapi.co/api/v2/pokemon/' + poke2
-  let pokeStore = []
-  let pokeImg = []
+  const url = 'http://jservice.io/api/random'
 
   fetch(url)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-      console.log(data.types[0].type.name)
-      pokeStore.push(data.types[0].type.name)
-      pokeImg.push(data.sprites.front_shiny)
+      console.log(data)
+      document.querySelector('h1').innerText = data[0].question
+      document.querySelector('h3').innerText = data[0].category.title
+      answer = data[0].answer
+
     })
     .catch(err => {
       console.log(`error ${err}`)
     });
 
-  fetch(url2)
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-      console.log(data.types[0].type.name)
-      pokeStore.push(data.types[0].type.name)
-      pokeImg.push(data.sprites.front_shiny)
-
-      console.log(pokeStore[0], pokeStore[1])
-      if (pokeStore[0] === 'grass' && pokeStore[1] === 'water') {
-        document.querySelector('#pokeImg1').src = pokeImg[0]
-        document.querySelector('#pokeImg2').src = pokeImg[1]
-        document.querySelector('h2').innerText = '2x >'
-      }
-    })
-    .catch(err => {
-      console.log(`error ${err}`)
-    });
-
+}
+function showAnswer() {
+  document.querySelector('h2').innerText = answer
 }
